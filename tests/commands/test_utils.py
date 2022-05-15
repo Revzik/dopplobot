8f1d0c9ab7c4af6b98965cf95458a6656217a823
Revzik <b.piekarz1997@gmail.com>
2022-05-15 23:53:55 +0200

    Fix failing test

--- tests/commands/test_utils.py
+++ tests/commands/test_utils.py
@@ -4,7 +4,7 @@ from dopplobot.commands import utils
 
 class UtilsTest(unittest.TestCase):
     def test_ping(self):
-        self.assertEqual("png", utils.ping())
+        self.assertEqual("pong", utils.ping())
 
 
 if __name__ == "__main__":

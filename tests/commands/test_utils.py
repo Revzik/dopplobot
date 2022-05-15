fe3b12442bc3dc0290c5d4753cbe63fe28651ec7
Revzik <b.piekarz1997@gmail.com>
2022-05-15 14:34:31 +0200

    Add simple test case, convert folders to packages

---
+++ tests/commands/test_utils.py
@@ -0,0 +1,12 @@
+from ast import Assert
+import unittest
+from dopplobot.commands import utils
+
+
+class UtilsTest(unittest.TestCase):
+    def test_ping(self):
+        self.assertEqual("pong", utils.ping())
+
+
+if __name__ == "__main__":
+    unittest.main()
